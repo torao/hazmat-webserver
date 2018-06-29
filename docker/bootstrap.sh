dc3383549d007bb5410a6df34890aec1d550775f
Takami Torao <t_takami@okwave.co.jp>
2018-06-29 17:49:55 +0900

    Dockerfile修正

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-if [ -f /opt/site/requirements.txt ]
+SITE=/opt/site
+
+bootstrap(){
+  echo -e "\n"
+  echo "=== BOOTSTRAP: `date` ==="
+  if [ -f $SITE/requirements.txt ]
+  then
+    pip install -r $SITE/requirements.txt
+  fi
+  if [ -f $SITE/package.json ]
+  then
+    cd $SITE
+    npm init
+  fi
+  if [ -f $SITE/scripts/bootstrap.sh ]
+  then
+    source $SITE/scripts/bootstrap.sh
+  fi
+}
+
+# bootstrap logging
+LOGDIR=$SITE/logs
+LOGFILE=$LOGDIR/bootstrap-`date "+%Y%m%d"`.log
+if [ -d $LOGDIR]
 then
-  pip install -r /opt/site/requirements.txt &
+  mkdir -p $LOGDIR
 fi
 
-if [ -f /opt/site/package.json ]
-then
-  cd /opt/site && npm init &
-fi
-
-cd /opt/site
+bootstrap > $LOGFILE 2>&1 &
+cd $SITE
 /opt/webserver/bin/hazmat-webserver $*
