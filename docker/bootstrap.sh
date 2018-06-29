25445b271ae35dfa6a18781e978cad673940b0e2
Takami Torao <t_takami@okwave.co.jp>
2018-06-29 12:51:10 +0900

    起動スクリプトに ^M が混入していたのを削除

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 if [ -f /opt/site/requirements.txt ]
+then
   pip install -r /opt/site/requirements.txt &
 fi
 
 if [ -f /opt/site/package.json ]
+then
   cd /opt/site && npm init &
 fi
 
