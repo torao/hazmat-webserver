2786070b47a0d3d29b8311bb2ff7a01f3e41a681
TAKAMI Torao <koiroha@gmail.com>
2022-02-16 00:16:22 +0900

    1.2.7: Mermaid 追加

--- docker-build.bat
+++ docker-build.bat
@@ -5,5 +5,9 @@ set VERSION=1.2.7
 
 call sbt universal:packageBin
 move /y target\universal\hazmat-webserver-%VERSION%.zip docker\
+if not errorlevel 0 (
+  echo "ERROR: failed to build hazmat-webserver-%VERSION%.zip"
+  exit 1
+)
 docker build -t torao/hazmat-webserver:%VERSION% docker --build-arg VERSION=%VERSION%
 docker tag torao/hazmat-webserver:%VERSION% torao/hazmat-webserver:latest
